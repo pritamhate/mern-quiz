3ed691bf8db194bedc6e7dc063f1a6cb00a675cf
pritamhate <pritam.hate@outlook.com>
2023-04-26 20:57:47 +0530

    questions data added and fetch into the quiz

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Questions from "./Questions";
 
 export default function Quiz() {
+
+    /** next button event handler **/
+    function onNext() {
+        console.log('On Next Click');
+    }
+
+    /** prev button event handler **/
+    function onPrev() {
+        console.log('On Prev Click');
+    } 
+
     return(
-        <div>Quiz</div>
+        <div className="container">
+            <h1 className="title text-light">Quiz App</h1>
+
+            {/** Display Questions **/}
+            <Questions></Questions>
+
+            <div className="grid">
+                <button className="btn prev" onClick={onPrev}>Prev</button>
+                <button className="btn next" onClick={onNext}>Next</button>
+            </div>
+        </div>
     )
 }
---
+++ client/src/database/data.js
@@ -0,0 +1,92 @@
+export default [
+    {
+        id: 1,
+        question: "Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 2,
+        question: "02 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 3,
+        question: "03 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 4,
+        question: "04 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 5,
+        question: "05 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 6,
+        question: "06 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 7,
+        question: "07 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 8,
+        question: "08 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 9,
+        question: "09 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 10,
+        question: "10 Javascript is an _______ language",
+        options: [
+            'Object Oriented',
+            'Object Based',
+            'Procedural'
+        ]
+    }
+]
